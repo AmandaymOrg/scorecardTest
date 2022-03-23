1936167a0b99881c518901d8fe70aa3606d86ced
Amandaym <570718664@qq.com>
2022-03-23 19:58:15 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/test/models"
 	_ "github.com/test/routers"
 )
 
 func main() {
+	fmt.Println("hello scorecard...")
 	models.LinkedDome()
 	beego.Run()
 }
